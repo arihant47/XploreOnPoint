cc8337624c9cf00783ba2b13afc6804020701e41
Arihant Kharai <kharaiarihant47@gmail.com>
2020-08-27 05:56:27 +0000

    connect-flash Config.

--- routes/comments.js
+++ routes/comments.js
@@ -23,8 +23,9 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 	//Lookup campground using ID
 	Campground.findById(req.params.id, function(err, campground){
 		if(err){
-		console.log(err);
-		res.redirect("/campground");
+			req.flash("error", "Something went wrong");
+			console.log(err);
+			res.redirect("/campground");
 		} else {
 			Comment.create(req.body.comment, function(err, comment){
 				if(err) {
@@ -38,6 +39,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 					campground.comments.push(comment);
 					campground.save();
 					console.log(comment);
+					req.flash("success", "Successfully added comment");
 					res.redirect("/campgrounds/"+campground._id);
 				}
 			});
@@ -47,12 +49,18 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 
 //Comment Edit route
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
-	Comment.findById(req.params.comment_id, function(err, foundComment){
-		if(err) {
-			res.render("back");
-		} else {
-			res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err || !foundCampground){
+			req.flash("error", "No campground found");
+			return res.redirect("back");
 		}
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err) {
+				res.render("back");
+			} else {
+				res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+			}
+		});	
 	});	
 });
 
@@ -73,6 +81,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
 		if(err) {
 			res.redirect("back");
 		} else {
+			req.flash("success", "Comment deleted!");
 			res.redirect("/campgrounds/"+req.params.id);
 		}
 	});
